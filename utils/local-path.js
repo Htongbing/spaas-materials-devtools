f8171365a4b7e56d1f3057ad05eef1934a1d017e
barret <47966933+Barretem@users.noreply.github.com>
2019-07-02 11:51:46 +0800

    feat: init project

---
+++ utils/local-path.js
@@ -0,0 +1,11 @@
+/**
+ * 判断是否是本地路径
+ * @param {string} templatePath
+ */
+function isLocalPath(templatePath) {
+  return /^[./]|(^[a-zA-Z]:)/.test(templatePath);
+}
+
+module.exports = {
+  isLocalPath,
+};
