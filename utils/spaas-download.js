f1cd46edc7bf1612727464bfd5f730af344bf315
Htongbing <36433396+Htongbing@users.noreply.github.com>
2019-10-12 15:58:10 +0800

    feat: 完成初始化脚手架功能 (#2)
    * feat: 下载github脚手架仓库

---
+++ utils/spaas-download.js
@@ -0,0 +1,19 @@
+const download = require('download-git-repo');
+const ora = require('ora');
+const { spaasCliGithubDepository } = require('../config/const');
+const logger = require('../utils/logger');
+
+module.exports = dir => {
+  return new Promise((resolve, reject) => {
+
+    const spinner = ora('downloading');
+    spinner.start();
+
+    download(spaasCliGithubDepository, `./${dir}`, err => {
+      spinner.stop();
+      if (err) return reject(err);
+      logger.info('init success');
+      resolve();
+    });
+  });
+};
